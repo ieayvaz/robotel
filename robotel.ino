f31b50bd543c550641c678fd88f0ddbb4de63c35
Ismail Emir Ayvaz <ieayvaz@protonmail.ch>
2021-05-03 03:09:47 +0300

    Led related lines deleted

--- robotel.ino
+++ robotel.ino
@@ -8,19 +8,13 @@ AccelStepper stepper2(1, 10, 11); // direction Digital 11 (CCW), pulses Digital 
 //Pins
 const byte Analog_X_pin = A0; //x-axis readings
 const byte Analog_Y_pin = A1; //y-axis readings
-const byte Analog_R_pin = A2; //r-axis readings
-const byte LED_pin = 3; //PWM output for LED
 
 //Variables
 int Analog_X = 0; //x-axis value
 int Analog_Y = 0; //y-axis value
-int Analog_R = 0; //r-axis value
 
 int Analog_X_AVG = 0; //x-axis value averagen
 int Analog_Y_AVG = 0; //y-axis value average
-int Analog_R_AVG = 0; //r-axis value average
-
-int Analog_R_Value = 0; //this is used for the PWM value
 
 void setup()
 {
@@ -31,8 +25,6 @@ void setup()
   //PINS
   pinMode(Analog_X_pin, INPUT);
   pinMode(Analog_Y_pin, INPUT);  
-  pinMode(Analog_R_pin, INPUT); 
-  pinMode(LED_pin, OUTPUT);
   //----------------------------------------------------------------------------  
   InitialValues(); //averaging the values of the 3 analog pins (values from potmeters)
   //----------------------------------------------------------------------------  
@@ -61,9 +53,8 @@ void ReadAnalog()
 {
   //Reading the 3 potentiometers in the joystick: x, y and r.
   Analog_X = analogRead(Analog_X_pin);  
-  Analog_Y = analogRead(Analog_Y_pin);    
-  Analog_R = analogRead(Analog_R_pin);    
-
+  Analog_Y = analogRead(Analog_Y_pin);
+  
   //if the value is 25 "value away" from the average (midpoint), we allow the update of the speed
   //This is a sort of a filter for the inaccuracy of the reading
   if(abs(Analog_X-Analog_X_AVG)>25) 
@@ -83,12 +74,7 @@ void ReadAnalog()
   {
     stepper2.setSpeed(0);
   }
-  //----------------------------------------------------------------------------  
-  if(abs(Analog_R-Analog_R_AVG)>25) 
-  {
-    Analog_R_Value = map(Analog_R, 0, 1023, 0, 255); //10 bit ADC (0-1023) and 8 bit PWM (0-255)
-    analogWrite(LED_pin, Analog_R_Value); //modify the PWM value     
-  }
+  //----------------------------------------------------------------------------
 }
 
 // 
@@ -98,7 +84,6 @@ void InitialValues()
   //Set the values to zero before averaging
   float tempX = 0;
   float tempY = 0;
-  float tempR = 0;
   //----------------------------------------------------------------------------  
   //read the analog 50x, then calculate an average. 
   //they will be the reference values
@@ -108,19 +93,14 @@ void InitialValues()
      delay(10); //allowing a little time between two readings
      tempY += analogRead(Analog_Y_pin);        
      delay(10);
-     tempR += analogRead(Analog_R_pin);
-     delay(10);
   }
   //----------------------------------------------------------------------------  
   Analog_X_AVG = tempX/50; 
-  Analog_Y_AVG = tempY/50; 
-  Analog_R_AVG = tempR/50; 
+  Analog_Y_AVG = tempY/50;  
   //----------------------------------------------------------------------------  
   Serial.print("AVG_X: ");
   Serial.println(Analog_X_AVG);
   Serial.print("AVG_Y: ");
   Serial.println(Analog_Y_AVG);
-  Serial.print("AVG_R: ");
-  Serial.println(Analog_R_AVG);
   Serial.println("Calibration finished");  
 }
